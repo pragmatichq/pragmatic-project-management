d918555d59765fc507c752bee48b9e0a9a362e2a
Kyle Morck <kyle.morck@gmail.com>
2024-01-24 18:13:59 -0700

    Added Clerk integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,200 @@
 "use client";
-import { useQuery } from "convex/react";
-import { api } from "../convex/_generated/api";
+
+import { Authenticated, Unauthenticated } from "convex/react";
+
+import {
+  useAuth,
+  OrganizationList,
+  OrganizationSwitcher,
+  UserButton,
+  SignInButton,
+  useOrganization,
+} from "@clerk/nextjs";
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
-  const tasks = useQuery(api.tasks.get);
+  const { organization, memberships } = useOrganization({
+    memberships: {
+      keepPreviousData: true,
+    },
+  });
+  const { orgId, userId } = useAuth();
+  const memberList = memberships!.data?.find(
+    (item) => item.publicUserData.userId === userId
+  );
+
+  console.log(memberList);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {tasks?.map(({ _id, text }) => (
-        <div key={_id}>{text}</div>
-      ))}
+    <main>
+      {memberships != null &&
+        memberships.data?.map((membership) => (
+          <li key={membership.id}>
+            {membership.publicUserData.firstName}{" "}
+            {membership.publicUserData.lastName}
+            {membership.publicUserData.imageUrl}
+          </li>
+        ))}
+
+      <div className="hidden flex-col md:flex">
+        <div className="border-b">
+          <div className="flex h-16 items-center px-4">
+            <nav className="flex items-center space-x-4 lg:space-x-6 mx-6">
+              <Link
+                href="/examples/dashboard"
+                className="text-sm font-medium transition-colors hover:text-primary"
+              >
+                Overview
+              </Link>
+              <Link
+                href="/examples/dashboard"
+                className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+              >
+                Customers
+              </Link>
+              <Link
+                href="/examples/dashboard"
+                className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+              >
+                Products
+              </Link>
+              <Link
+                href="/examples/dashboard"
+                className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+              >
+                Settings
+              </Link>
+            </nav>
+            <div className="ml-auto flex items-center space-x-4">
+              <Unauthenticated>
+                <SignInButton mode="modal" />
+              </Unauthenticated>
+              <Authenticated>
+                {orgId != null && <OrganizationSwitcher hidePersonal />}
+                <UserButton afterSignOutUrl="/" />
+              </Authenticated>
+            </div>
+          </div>
+        </div>
+        {orgId == null ? (
+          <OrganizationList hidePersonal />
+        ) : (
+          <div className="flex-1 space-y-4 p-8 pt-6">
+            <div className="flex items-center justify-between space-y-2">
+              <h2 className="text-3xl font-bold tracking-tight">
+                {organization?.name}
+              </h2>
+            </div>
+            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+              <Card>
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                  <CardTitle className="text-sm font-medium">
+                    Total Revenue
+                  </CardTitle>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    className="h-4 w-4 text-muted-foreground"
+                  >
+                    <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
+                  </svg>
+                </CardHeader>
+                <CardContent>
+                  <div className="text-2xl font-bold">$45,231.89</div>
+                  <p className="text-xs text-muted-foreground">
+                    +20.1% from last month
+                  </p>
+                </CardContent>
+              </Card>
+              <Card>
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                  <CardTitle className="text-sm font-medium">
+                    Subscriptions
+                  </CardTitle>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    className="h-4 w-4 text-muted-foreground"
+                  >
+                    <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+                    <circle cx="9" cy="7" r="4" />
+                    <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
+                  </svg>
+                </CardHeader>
+                <CardContent>
+                  <div className="text-2xl font-bold">+2350</div>
+                  <p className="text-xs text-muted-foreground">
+                    +180.1% from last month
+                  </p>
+                </CardContent>
+              </Card>
+              <Card>
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                  <CardTitle className="text-sm font-medium">Sales</CardTitle>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    className="h-4 w-4 text-muted-foreground"
+                  >
+                    <rect width="20" height="14" x="2" y="5" rx="2" />
+                    <path d="M2 10h20" />
+                  </svg>
+                </CardHeader>
+                <CardContent>
+                  <div className="text-2xl font-bold">+12,234</div>
+                  <p className="text-xs text-muted-foreground">
+                    +19% from last month
+                  </p>
+                </CardContent>
+              </Card>
+              <Card>
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                  <CardTitle className="text-sm font-medium">
+                    Active Now
+                  </CardTitle>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    className="h-4 w-4 text-muted-foreground"
+                  >
+                    <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
+                  </svg>
+                </CardHeader>
+                <CardContent>
+                  <div className="text-2xl font-bold">+573</div>
+                  <p className="text-xs text-muted-foreground">
+                    +201 since last hour
+                  </p>
+                </CardContent>
+              </Card>
+            </div>
+          </div>
+        )}
+      </div>
     </main>
   );
 }
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://helping-snail-89.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
