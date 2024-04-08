c14bfb2ef9e3c833e02c07675a1e5536242be65d
Kyle Morck <kyle.morck@gmail.com>
2024-04-08 12:57:56 -0600

    Updated dashboard heading

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -11,7 +11,7 @@ export default function actionListPage() {
   }, []);
   return (
     <div className="flex flex-col space-y-2 p-4">
-      <h2 className="text-4xl font-bold tracking-tight">Inbox</h2>
+      <h2 className="text-4xl font-bold tracking-tight">Dashboard</h2>
     </div>
   );
 }
